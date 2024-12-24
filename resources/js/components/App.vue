ca2159e0e8773b619f7f42c4c8d8c3d349ce0ea2
Lĩnh <150920038+vuhonglinh@users.noreply.github.com>
2024-12-24 13:36:26 +0700

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hello from Vue!</h1>
+    </div>
+</template>
+
+<script setup>
+// Bạn có thể sử dụng Vue Composition API tại đây
+</script>
+
+<style scoped>
+/* CSS của component */
+</style>
