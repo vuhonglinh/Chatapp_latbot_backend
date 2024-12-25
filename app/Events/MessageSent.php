ae2b95b7c6d444f0c512d0a0518dc13628a9b6d9
Lĩnh <150920038+vuhonglinh@users.noreply.github.com>
2024-12-25 17:22:59 +0700

    update

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use App\Http\Resources\MessageResource;
+use App\Models\Message;
+use App\Models\Room;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use ReflectionProperty;
+
+class MessageSent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    private $room, $message;
+    public function __construct(Room $room,Message $message)
+    {
+        $this->room = $room;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-room-'.$this->room->id),
+        ];
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'data' => new MessageResource($this->message)
+        ];
+    }
+
+
+}
--- routes/channels.php
+++ routes/channels.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
+});
+
+
+Broadcast::private('channel-room-{id}', function ($user, $id) {
+    return $user != null;
 });
