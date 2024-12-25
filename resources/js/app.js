b54590d23ecde4e47ebad3a82d4c20bd2092df72
Lĩnh <150920038+vuhonglinh@users.noreply.github.com>
2024-12-25 15:05:46 +0700

    update

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import App from './components/App.vue';
-
-createApp(App).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,17 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.ts',
+            ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
