f361f486f75e600cc1f753c2b1d982168d26dfab
Lĩnh <150920038+vuhonglinh@users.noreply.github.com>
2024-12-26 10:05:48 +0700

    update

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Message\MessageRepository;
+use App\Repositories\Room\RoomRepository;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChatController extends Controller
 {
-    //
+    private $roomRepository, $messageRepository;
+
+    public function __construct(RoomRepository $roomRepository, MessageRepository $messageRepository)
+    {
+        $this->roomRepository = $roomRepository;
+        $this->messageRepository = $messageRepository;
+    }
+
+    public function send(Request $request,$room_id){
+        try{
+            $message = $this->roomRepository->
+            return response()->json([
+                'message' => 'Send message successfully'
+            ],Response::HTTP_OK);
+        }catch (\Throwable $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ],Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
--- app/Repositories/Room/RoomRepository.php
+++ app/Repositories/Room/RoomRepository.php
@@ -7,83 +7,50 @@ use App\Repositories\BaseRepository;
 
 class RoomRepository extends BaseRepository
 {
-    /**
-     * Thiết lập Model tương tác
-     *
-     * @return string
-     */
     public function model()
     {
         return Room::class;
     }
 
-    /**
-     * Lấy danh sách phân trang
-     *
-     * @param int $limit
-     * @param array $columns
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
-     */
     public function paginateRooms($limit = 25, array $columns = ['*'])
     {
         return $this->paginate($limit, $columns);
     }
 
-    /**
-     * Thêm mới
-     *
-     * @param array $data
-     * @return \App\Models\Room
-     */
     public function createRoom(array $data)
     {
         return $this->create($data);
     }
 
-    /**
-     * Cập nhật
-     *
-     * @param int $id
-     * @param array $data
-     * @return \App\Models\Room
-     */
+
     public function updateRoomById($id, array $data)
     {
         return $this->updateById($id, $data);
     }
 
-    /**
-     * Xóa
-     *
-     * @param int $id
-     * @return bool|null
-     * @throws \Exception
-     */
     public function deleteRoomById($id)
     {
         return $this->deleteById($id);
     }
 
-    /**
-     * Lấy tất cả
-     *
-     * @param array $columns
-     * @return \Illuminate\Database\Eloquent\Collection|\App\Models\Room[]
-     */
+
     public function getAllRooms(array $columns = ['*'])
     {
         return $this->all($columns);
     }
 
-    /**
-     * Lấy chi tiết
-     *
-     * @param int $id
-     * @param array $columns
-     * @return \App\Models\Room
-     */
+
     public function getRoomById($id, array $columns = ['*'])
     {
         return $this->getById($id, $columns);
+    }
+
+    public function sendMessage($request, $room_id)
+    {
+        try{
+//            $message = $this->model->cea
+        }catch (\Throwable $e){
+            throw $e;
+        }
     }
 }
